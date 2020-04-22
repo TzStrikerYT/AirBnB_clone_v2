c65aacc61a5d53dae8a8fa818ba81a198df074ca
Michael <maicol.asus@gmail.com>
2020-04-22 02:56:11 +0000

    [add] add task 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" my first flask service """
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_flask():
+    """ Return Hello HBNB """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hello_hbnb():
+    """ Return HBNB """
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def content(text):
+    """ Return HBNB """
+    content = str(text).replace(" ", " ")
+    return "C {:s}".format(text)
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", 5000)
