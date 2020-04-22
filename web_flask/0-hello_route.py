a1fd61b6a5986954a19246bc61061c6cd9b4cb6f
Michael <maicol.asus@gmail.com>
2020-04-22 01:35:21 +0000

    [add] add task 0

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" my first flask service """
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_flask():
+    """ Return Hello HBNB """
+    return "Hello HBNB!"
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", 5000)
