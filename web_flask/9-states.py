0ece1d9693589e377ccc513bf20074fb91b40824
Michael <maicol.asus@gmail.com>
2020-04-23 03:31:38 +0000

    [fix] fix vars

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,38 +1,33 @@
 #!/usr/bin/python3
-
 from flask import Flask, render_template
 import models
-from models.state import State
+from models import State
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
-
 @app.teardown_appcontext
-def kill(order):
-    """ Close DB session """
+def closedb(foo):
+    """kill session DB"""
     models.storage.close()
 
 
-@app.route("/states", defaults={"id": None})
-@app.route("/states/<id>")
-def states(id):
-    """ route to list of states """
-    s_list = None
-    res = s_list
-
+@app.route('/states', defaults={'id': None})
+@app.route('/states/<id>')
+def state(id):
+    """ List of states """
+    state = states = None
     if not id:
-        s_list = list(models.storage.all(State).values())
+        states = list(storage.all(State).values())
     else:
-        s_list = models.storage.all(State)
+        states = models.storage.all(State)
         key = "State.{}".format(id)
-
-        if key in s_list:
-            res = s_list[key]
+        if key in states:
+            state = states[key]
         else:
-            res = None
-        s_list = []
-    return render_template("9-states.html", **locals())
+            state = None
+        states = []
+    return render_template('9-states.html', **locals())
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     models.storage.reload()
     app.run("0.0.0.0", 5000)
